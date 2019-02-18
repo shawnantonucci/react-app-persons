0e94fdc86d03c6b07b33d29c8f7bf8f5f266fb7c
shawn <shawnantonucci2018@gmail.com>
2019-02-18 10:38:22 -0800

    added in Context api for auth

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 import Aux from "../../../hoc/Auxilary";
-import withClass from '../../../hoc/withClass';
+import withClass from "../../../hoc/withClass";
 import classes from "./Person.css";
+import AuthContext from "../../../context/auth-context";
 
 class Person extends Component {
   constructor(props) {
@@ -20,7 +21,11 @@ class Person extends Component {
     console.log("[Person.js] rendering...");
     return (
       <Aux>
-        {this.props.isAuth ? <p>Authenticated!</p> : <p>Please log in</p> }
+        <AuthContext.Consumer>
+          {context =>
+            context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>
+          }
+        </AuthContext.Consumer>
         <p key="i1" onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
